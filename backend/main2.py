516c5c0f0de3785cfd6ce4a6839da0f5105d73fb
Hongjin Zhu <hz2291@nyu.edu>
2024-06-06 17:34:02 -0400

    Initialized FastAPI

---
+++ backend/main2.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+from fastapi.middlerware.cors import CORSMiddleware
+
+class VideoAnalysisRequest(BaseModel):
+    youtube_link: HttpUrl
+    # advanced settings
+
+app = FastAPI()
+
+@app.post("/analyse_video")
+def analyze_video(request: VideoAnalysisRequest):
+    # Doing the Analysis
+    from langchain_community.document_loaders import YoutubeLoader
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+    loader = YoutubeLoader.from_youtube_url(str(request.youtube_link), add_video_info=True)
+    docs = loader.load()
+    print(f"On load: {type(docs)}")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    result = text_splitter.split_documents(docs)
+
+    print(f"{type(result)}")
+    author = result[0].metadata['author']
+    length = result[0].metadata['length']
+    title = result[0].metadata['title']
+    total_size = len(result)
+    
+    return {
+        # 'youtube_link': request.youtube_link
+        "author": author,
+        "length": length,
+        "title": title,
+        "total_size": total_size
+    }
+
+@app.get("/root")
+def health():
+    return {"status": "ok"}
