02d5ab1d774d0989053b4c4bb1267d0b25d80f0b
Hongjin Zhu <hz2291@nyu.edu>
2024-06-10 17:30:40 -0400

    added semantic extraction algo

--- backend/main2.py
+++ backend/main2.py
@@ -18,19 +18,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+genai_processor = GeminiProcessor(
+    model_name = 'gemini-pro',
+    project = 'sample-mission-424819'
+)
+
 @app.post("/analyze_video")
 def analyze_video(request: VideoAnalysisRequest):
     # Doing the Analysis
-    processor = YoutubeProcessor()
+    processor = YoutubeProcessor(genai_processor=genai_processor)
     result = processor.retrieve_youtube_documents(str(request.youtube_link), verbose=True)
-    genai_processor = GeminiProcessor(
-        model_name = 'gemini-pro',
-        project = 'sample-mission-424819'
-    )
-    summary = genai_processor.generate_document_summary(result, verbose=True)
+    
+    # summary = genai_processor.generate_document_summary(result, verbose=True)
+    # find key concepts
+    key_concepts = processor.find_key_concepts(result, group_size=2)
     
     return {
-        "summary": summary
+        # "summary": summary
+        "key_concepts": key_concepts
     }
 
 @app.get("/root")
--- backend/services/genai2.py
+++ backend/services/genai2.py
@@ -2,20 +2,50 @@ from langchain_community.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_vertexai import VertexAI
 from langchain.chains.summarize import load_summarize_chain
+from vertexai.generative_models import GenerativeModel
+from langchain.prompts import PromptTemplate
 import logging
+from tqdm import tqdm
 
 # configure log
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+class GeminiProcessor:
+    def __init__(self, model_name, project):
+        self.model = VertexAI(model_name=model_name, project=project)
+    
+    def generate_document_summary(self, documents: list, **args):
+        # 'stuff': putting all documents into 1 context window, may cause crash down
+        chain_type = 'map_reduce' if len(documents) > 10 else 'stuff'
+
+        chain = load_summarize_chain(
+            llm = self.model,
+            chain_type = chain_type,
+            **args
+        )
+        return chain.run(documents)
+    
+    def count_total_tokens(self, docs:list):
+        temp_model = GenerativeModel('gemini-1.0-pro')
+        total = 0
+        logger.info('Counting total tokens...')
+        for doc in tqdm(docs):
+            total += temp_model.count_tokens(doc.page_content).total_tokens
+        return total
+    
+    def get_model(self):
+        return self.model
+    
 class YoutubeProcessor:
     # retrieve the full transcript
 
-    def __init__(self):
+    def __init__(self, genai_processor = GeminiProcessor):
         self.text_splitter = RecursiveCharacterTextSplitter(
             chunk_size = 1000,
             chunk_overlap = 0
         )
+        self.GeminiProcessor = genai_processor
 
     def retrieve_youtube_documents(self, video_url: str, verbose = False):
         loader = YoutubeLoader.from_youtube_url(video_url, add_video_info=True)
@@ -33,17 +63,41 @@ class YoutubeProcessor:
 
         return result
     
-class GeminiProcessor:
-    def __init__(self, model_name, project):
-        self.model = VertexAI(model_name=model_name, project=project)
-    
-    def generate_document_summary(self, documents: list, **args):
-        # 'stuff': putting all documents into 1 context window, may cause crash down
-        chain_type = 'map_reduce' if len(documents) > 10 else 'stuff'
+    def find_key_concepts(self, documents:list, group_size: int=2):
+        # iterate through all documents of group size and find key concepts
+        if group_size > len(documents):
+            raise ValueError("Group size is larger than the number of documents")
+        
+        # find number of documents in each group
+        num_docs_per_group = len(documents) // group_size + (len(documents) % group_size > 0)
+        
+        # split the document in chunks of size num_docs_per_group
+        groups = [documents[i:i+num_docs_per_group] for i in range (0, len(documents), num_docs_per_group)]
+        batch_concepts = []
+        logger.info("Fining key concepts...")
 
-        chain = load_summarize_chain(
-            llm = self.model,
-            chain_type = chain_type,
-            **args
-        )
-        return chain.run(documents)
+        for group in tqdm(groups):
+            # combine content of documents per group
+            group_content = ""
+
+            for doc in group:
+                group_content += doc.page_content
+
+            # prompt for finding concepts
+            prompt = PromptTemplate(
+                template = """
+                Find and define key concepts or terms found in the text:
+                {text}
+
+                Respond in the following format as a string separating each concept with a comma:
+                "concept": "definition"
+                """,
+                input_variables = ['text']
+            )
+            # create chain
+            chain = prompt | self.GeminiProcessor.model
+            # run chain
+            concept = chain.invoke({"text": group_content})
+            batch_concepts.append(concept)
+
+        return batch_concepts
